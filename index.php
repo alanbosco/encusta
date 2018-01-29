9762f4ffa3bdc5e116df92d48c631066c11c6f85
ala.n <justafreak15@gmail.com>
2018-01-29 12:55:29 +0530

    logos added

--- index.php
+++ index.php
@@ -5,3 +5,18 @@
  * Date: 29-01-2018
  * Time: 11:47 AM
  */
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<div class="nav">
+<img scr="ilahia.png"></img>
+<img scr="code.png" style="margin-right: auto;"></img>
+</div>
+<div>
+<img scr="enq.png" style=""></img>
+    <button>Start</button>
+</div>
+</body>
+</html>
+
